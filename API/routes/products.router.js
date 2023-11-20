92ce4b40a8f19ad8592e49acd591266bf748f88a
MinJae-Mun <80499417+MinJae-Mun@users.noreply.github.com>
2023-11-20 16:15:58 +0900

    Update products.router.js

--- API/routes/products.router.js
+++ API/routes/products.router.js
@@ -56,3 +56,36 @@ router.get("/products/:productId", async (req, res) => {
         res.status(500).json({ message: "ERROR"});
     }
 });
+
+router.put("/products/:productId", async (req, res) => {
+    try {
+        if (!req.body || !req.params) {
+            return res
+                .status(400)
+                .json({ message: "데이터 형식이 올바르지 않습니다." });
+        }
+
+        const { title, content, password, status } = req.body;
+        const product = await Product.findById(req.params.productId);
+
+        if (!product) {
+            return res.status(404).json({ message: "상품이 존재하지 않습니다." });
+        }
+
+        if (password != product.password) {
+            return res
+                .status(404)
+                .json({ message: "권한이 존재하지 않습니다." });
+        }
+
+        product.title = title;
+        product.content = content;
+        product.status = status;
+
+        await product.save();
+        res.json({ message: "수정 완료" });
+    }
+    catch (error) {
+        res.status(500).json({ message: "ERROR"});
+    }
+})
